4e9c30aa80005d860e6c3744fb3b3b8305424211
shlathsaheb <shabbir.husain.1987@gmail.com>
2017-08-22 03:30:41 +0200

    Add files via upload

---
+++ DemoWebApi/DemoWebApi/App_Code/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.App_Code
+{
+    
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsActive { get; set; }
+        public List<Phone> Phones { get; set; }
+        public List<Email> Emails { get; set; }
+    }
+
+    public class Phone
+    {
+        public int ID { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+
+    public class Email
+    {
+        public int ID { get; set; }
+        public string EmailAddress { get; set; }
+    }
+
+    
+}
---
+++ DemoWebApi/DemoWebApi/Controllers/PersonController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api/person")]
+    public class PersonController : ApiController
+    {
+        List<Person> persons = new List<Person> {
+                new Person{ DateOfBirth = new DateTime(1987, 04, 03),
+                 Designation = "Developer", ID = 1, Name = "Shabbir", IsActive = true,
+                Emails = new List<Email> { new Email { ID = 1, EmailAddress = "s.lathsaheb@iaea.org"
+                }}, Phones = new List<Phone>{ new Phone { ID =1, PhoneNumber = 123456789 } }
+                }
+            };
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public async Task<IHttpActionResult> Get(int id)
+        {
+            return Ok(persons.FirstOrDefault(p=> p.ID == id));
+        }
+    }
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool IsActive { get; set; }
+        public List<Phone> Phones { get; set; }
+        public List<Email> Emails { get; set; }
+    }
+
+    public class Phone
+    {
+        public int ID { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+
+    public class Email
+    {
+        public int ID { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
